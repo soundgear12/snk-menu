2f0188d57ab7391946a29ed3e2504473a1b49256
Antonio <antonio.rodriguez024@gmail.com>
2022-03-24 00:39:56 -0500

    menu cell template completed

--- src/App.js
+++ src/App.js
@@ -3,21 +3,53 @@ import './App.css';
 
 function App() {
   return (
-    <div className="menu">
-      <div className='cell'>
-        Arcade Mode
+    <div>
+      <div className="row-1">
+        <div className='cell'>
+          Arcade Mode
+        </div>
+
+        <div className='cell'>
+          Survival Mode
+        </div>
+
+        <div className='cell'>
+          VS Mode
+        </div>
+
       </div>
 
-      <div className='cell'>
-        Survival Mode
+      <div className="row-2">
+        <div className='cell'>
+          Training Mode
+        </div>
+
+        <div className='cell'>
+          Mark of the Millenium
+        </div>
+
+        <div className='cell'>
+          Color Edit Mode
+        </div>
       </div>
 
-      <div className='cell'>
-        Arcade Mode
+      <div className="row-3">
+        <div className='cell'>
+          Replay Mode
+        </div>
+
+        <div className='cell'>
+          Option Mode
+        </div>
+
+        <div className='cell'>
+          Capcom vs SNK2
+        </div>
       </div>
-     
-      
     </div>
+
+
+
   );
 }
 
