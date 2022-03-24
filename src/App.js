c22b4546da35a7e1203a5768e9bc1d0ca2ac1e10
Antonio <antonio.rodriguez024@gmail.com>
2022-03-24 02:03:18 -0500

    added menu component

--- src/App.js
+++ src/App.js
@@ -1,70 +1,11 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import MenuComponent from './components/MenulComponent';
 
-function App() {
-  return (
-    <div>
-      <div className="row-1">
-        <div className='cell'>
-          <div className='cell-text'>
-            <p>ARCADE <br></br>MODE</p>
-          </div>
-        </div>
-
-        <div className='cell'>
-          <div className='cell-text'>
-            <p>SURVIVAL <br></br>MODE</p>
-          </div>
-        </div>
-
-        <div className='cell'>
-        <div className='cell-text'>
-            <p>VS <br></br>MODE</p>
-          </div>
-        </div>
-
-      </div>
-
-      <div className="row-2">
-        <div className='cell'>
-        <div className='cell-text'>
-            <p>TRAINING <br></br>MODE</p>
-          </div>
-        </div>
-
-        <div className='cell'>
-          Mark of the Millenium
-        </div>
-
-        <div className='cell'>
-        <div className='cell-text'>
-            <p>COLOR<br></br>MODE</p>
-          </div>
-        </div>
-      </div>
-
-      <div className="row-3">
-        <div className='cell'>
-        <div className='cell-text'>
-            <p>REPLAY <br></br>MODE</p>
-          </div>
-        </div>
-
-        <div className='cell'>
-        <div className='cell-text'>
-            <p>OPTION <br></br>MODE</p>
-          </div>
-        </div>
-
-        <div className='cell'>
-          Capcom vs SNK2
-        </div>
-      </div>
-    </div>
-
-
-
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <MenuComponent />
+    )
+  }
 }
-
-export default App;
